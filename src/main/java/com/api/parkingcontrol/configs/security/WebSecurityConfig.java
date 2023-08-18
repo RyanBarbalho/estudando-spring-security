836b66204175dc5c7ffcf966e9770579f17dbc76
RyanBarbalho <rgtbc@ic.ufal.br>
2023-08-18 09:32:13 -0300

    configuração de role admin e criação de user model para autenticação e configuração de senha

--- src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.api.parkingcontrol.configs.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -12,8 +16,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeHttpRequests()
                 .anyRequest()   //para todas as requisiçoes
-                .authenticated();
-        http.cors().and().csrf().disable();
+                .authenticated()//deve estar autenticado
+                .and()
+                .csrf().disable();//desbilitar bloqueio de solicitaçoes json para post,delete,etc
+                //se csrf estiver habilitado é preciso enviar o token csrf gerado pelo spring
+                //para ser autenticado
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("ryan")
+                .password(passwordEncoder().encode("123456"))
+                .roles("ADMIN");
+    }
+
+    @Bean //
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
